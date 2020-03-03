423e3d8ecfa4526e6fa948267b7cacdbbcca8612
Cole <alibilge@yahoo.ca>
2020-03-03 14:05:41 -0700

    update

--- js/day-2.js
+++ js/day-2.js
@@ -99,4 +99,77 @@ console.log('Type is: '+(typeof myString)); // { another: 'string test!?'}
   newParagraph.textContent = 'This paragraph was created completely in Javascript! Wowie! zounds! Gadzooks!'; // Populate your element
   //Add your element to the document body!
   document.body.appendChild( newParagraph );
+
+  var otherNewParagraph = document.createElement( 'p' );
+  otherNewParagraph.textContent = 'Second paragtaph'; 
   
+  // Please never actually do inline styles like this. Apply aclass or ID and use your CSS file  to apply styles instead!
+  otherNewParagraph.style.color='darkgray';
+
+  var myFirstLink = document.createElement('A'); // An anchor (link).
+  myFirstLink.textContent = 'Click Here!';
+  myFirstLink.href = ' ./index.html ';
+  myFirstLink.title = 'return to the home page.';
+  myFirstLink.style.color = 'red';
+
+  otherNewParagraph.appendChild(myFirstLink);
+
+
+// now, our paragraph with TEXT and a LINK both, will be added to the body!
+document.body.appendChild(otherNewParagraph);
+
+var targetLink = document.querySelector('.my-link');
+// targetLink.textContent = 'Go Back Home';
+
+
+/**
+ *  FORMS!
+ */
+
+ var calculatorForm=document.getElementById('calculator');
+
+ calculatorForm.addEventListener('submit', function(event){
+   // STOP the submission (event!)
+   event.preventDefault(); 
+
+   // Collect the form fiels (DOM objects.)
+  var num1Input=document.getElementById('num1');
+  var num2Input=document.getElementById('num2');
+
+  // Retrieve the values from the form fields
+
+  var operatorValue=operatorInput.value;
+  var num1Value=num1Input.value;
+  var num2Value=num2Input.value;
+
+ });
+
+//  var operatorInput=document.getElementById('operator');
+//  var num1Input=document.getElementById('num1');
+//  var num2Input=document.getElementById('num2');
+
+var result=0;
+
+ switch (operatorValue) {
+
+    case "subtract":
+        result=num1Value-num2Value;
+      break;
+     case "add":
+      result=num1Value+num2Value;
+       break;  //Switchs  execute starting at a first MATCHing value.
+               //However, they only STOP EXECUTING when they hit a break.
+               //No break,
+    
+    case "divide":
+      result=num1Value/num2Value;
+       break;
+    case "multiply":
+      result=num1Value*num2Value;
+     break;
+ } //end of switch
+
+ var resultElement=document.getElementById('result');
+ resultElement.textContent=result; // Show the result in our element
+
+} ); // End of event
